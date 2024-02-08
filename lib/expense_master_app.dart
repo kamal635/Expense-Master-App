3c3348d7d508f143c0bd9b91dc86c673d6f0fb19
kamal635 <kokoangry51@gmail.com>
2024-02-08 00:07:54 -0600

    initial project to start

---
+++ lib/expense_master_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ExpenseMasterApp extends StatelessWidget {
+  const ExpenseMasterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
