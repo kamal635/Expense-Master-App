9b060e5b1928d41148986f9f2e8c8845aa1929c8
kamal635 <kokoangry51@gmail.com>
2024-02-09 11:30:56 -0600

    add note to packages
    add sp to size fonts

--- lib/core/styling/app_fonts.dart
+++ lib/core/styling/app_fonts.dart
@@ -1,16 +1,17 @@
 import 'package:expense_master/core/styling/weight_fonts.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class AppFonts {
   //======================== Medium ========================
-  static const TextStyle medium_14 = TextStyle(
-    fontSize: 14,
+  static TextStyle medium_14 = TextStyle(
+    fontSize: 14.sp,
     fontWeight: WeightFonts.medium,
   );
 
   //======================== Semi Bold ========================
-  static const TextStyle semiBold_28 = TextStyle(
-    fontSize: 28,
+  static TextStyle semiBold_28 = TextStyle(
+    fontSize: 28.sp,
     fontWeight: WeightFonts.semiBold,
   );
 }
