8aa72b01b227f5722933888efd675f880417564b
kamal635 <kokoangry51@gmail.com>
2024-02-21 03:23:09 -0600

    change Split files and explain why used expanded

--- lib/core/routing/name_router.dart
+++ lib/core/routing/name_router.dart
@@ -1,5 +1,4 @@
 abstract class NameRouter {
   static const String onboardingView = "/onbordingView";
   static const String homeView = "/homeView";
-  static const String homeView2 = "/homeView2";
 }
--- lib/features/home/view/widgets/summary_section.dart
+++ lib/features/home/view/widgets/summary_section.dart
@@ -1,41 +1,46 @@
 import 'package:expense_master/core/Strings/app_strings.dart';
 import 'package:expense_master/core/icons/app_icon.dart';
 import 'package:expense_master/core/styling/app_color.dart';
-import 'package:expense_master/features/home/view/widgets/sum_home.dart';
+import 'package:expense_master/features/home/view/widgets/customs/custom_sum.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SummarySectionMonthlyAndDailyHomeView extends StatelessWidget {
   const SummarySectionMonthlyAndDailyHomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const IntrinsicHeight(
-      child: Row(
-        children: [
-          // Income
-          SumHomeView(
-            title: AppString.income,
-            icon: AppIcons.circleUp,
-            colorValueMonthly: AppColor.green,
-            iconColor: AppColor.green,
-            colorDivider: AppColor.green,
-          ),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: const IntrinsicHeight(
+        child: Row(
+          children: [
+            // Income
+            CustomSumHomeView(
+              title: AppString.income,
+              icon: AppIcons.circleUp,
+              colorValueMonthly: AppColor.green,
+              iconColor: AppColor.green,
+              colorDivider: AppColor.green,
+            ),
 
-          // Expense
-          SumHomeView(
-            title: AppString.income,
-            icon: AppIcons.circleDown,
-            colorValueMonthly: AppColor.darkRed,
-            iconColor: AppColor.darkRed,
-            colorDivider: AppColor.darkRed,
-          ),
+            // Expense
+            CustomSumHomeView(
+              title: AppString.expense,
+              icon: AppIcons.circleDown,
+              colorValueMonthly: AppColor.darkRed,
+              iconColor: AppColor.darkRed,
+              colorDivider: AppColor.darkRed,
+            ),
 
-          // Total
-          SumHomeView(
-            title: AppString.total,
-            icon: AppIcons.total,
-          ),
-        ],
+            // Total
+            CustomSumHomeView(
+              title: AppString.total,
+              icon: AppIcons.total,
+            ),
+          ],
+        ),
       ),
     );
   }
